ba5298de8fbf6f07e30b9669cc997a09777ddb02
Zhunaid Mohamed <zhunaid.m@gmail.com>
2023-09-24 12:38:45 +0200

    Rename Project

--- FutureForex/FutureForex/urls.py
+++ FutureForex/FutureForex/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for UploadDocuments project.
+URL configuration for FutureForex project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- FutureForex/manage.py
+++ FutureForex/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "UploadDocuments.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FutureForex.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
